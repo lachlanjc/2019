601bf52841ff81e2f952ecfb6473473c854662f5
Lachlan Campbell <lachlan@hackclub.com>
2019-12-27 15:26:10 -0500

    Add Top Songs

---
+++ components/top-songs.js
@@ -0,0 +1,70 @@
+import { Grid, Box, Text, Image } from '@theme-ui/components'
+import songs from '../data/top-songs.json'
+
+export default () => (
+  <Grid
+    as="ol"
+    gap={0}
+    sx={{
+      px: 3,
+      gridTemplateColumns: 'repeat(auto-fill, minmax(384px, 1fr))',
+      lineHeight: 'subheading'
+    }}
+  >
+    {songs.map((song, i) => (
+      <Grid
+        as="li"
+        gap={3}
+        sx={{
+          p: [1, 2],
+          gridTemplateColumns: '24px 64px 1fr',
+          alignItems: 'center',
+          borderBottom: '0.5px solid',
+          borderBottomColor: 'border',
+          overflow: 'hidden',
+          scrollSnapAlign: 'start'
+        }}
+        key={song.title}
+      >
+        <span>{i + 1}.</span>
+        {song.artwork ? (
+          <Image
+            src={song.artwork.replace('512x512', '128x128')}
+            loading="lazy"
+            width={64}
+            alt={`${song.album} artwork`}
+            sx={{ borderRadius: 'default' }}
+          />
+        ) : (
+          <Box
+            sx={{
+              bg: 'border',
+              borderRadius: 'default',
+              width: 64,
+              height: 64
+            }}
+          />
+        )}
+        <div>
+          <Text as="strong" sx={{ display: 'block' }}>
+            {song.title}
+          </Text>
+          <Text as="small" variant="caption">
+            {song.artist} –{' '}
+            {song.album.includes(' - Single') ? 'Single' : song.album}
+          </Text>
+        </div>
+      </Grid>
+    ))}
+    <style jsx>{`
+      ol {
+        line-height: 1.375;
+        scroll-snap-type: y mandatory;
+        overflow-y: scroll;
+        height: 100%;
+        max-height: 85vh;
+        text-align: left;
+      }
+    `}</style>
+  </Grid>
+)
