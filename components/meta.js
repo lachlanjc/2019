8ed772aa48b7e4023dde6cc9482f4c914ca2988a
Lachlan Campbell <lachlan@hackclub.com>
2019-12-27 14:34:16 -0500

    Redo site infra

--- components/meta.js
+++ components/meta.js
@@ -1,26 +1,60 @@
 import Head from 'next/head'
+import { palette } from './theme'
+
+const name = '2019 in Review – @lachlanjc'
+const makeTitle = title => (title === name ? title : `${title} – ${name}`)
 
 export default ({
-  title = 'One Day This Kid Will Talk',
-  description = 'January 2019, 111 hackers came together for Pennsylvania’s largest high school hackathon ever.',
-  image = 'https://2019.hackpenn.com/card.png',
-  url = 'https://hackpenn.com'
+  title = name,
+  description = 'Looking back at Lachlan Campbell’s 2019.',
+  image = 'https://hackathons.now.sh/card.png',
+  color = palette.primary
 }) => (
   <Head>
-    <title>{title}</title>
-    <meta property="og:title" content={title} />
-    <meta name="twitter:title" content={title} />
-    <meta name="og:url" content={url} />
-    <meta property="og:type" content="website" />
-    <meta property="og:site_name" content="Hack Pennsylvania" />
-    <meta name="twitter:site" content="@hackpenn" />
-    <meta name="description" content={description} />
-    <meta property="og:description" content={description} />
-    <meta name="twitter:description" content={description} />
-    <meta property="og:image" content={image} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={image} />
-    <meta name="theme-color" content="#2b34b6" />
+    <meta key="og_type" property="og:type" content="website" />
+    <meta key="og_site" property="og:site_name" content={name} />
+    <meta key="twitter_site" name="twitter:site" content="@lachlanjc" />
+    <title>{makeTitle(title)}</title>
+    <meta key="og_title" property="og:title" content={makeTitle(title)} />
+    <meta key="twitter_title" name="twitter:title" content={makeTitle(title)} />
+    <meta key="desc" name="description" content={description} />
+    <meta key="og_desc" property="og:description" content={description} />
+    <meta key="twitter_desc" name="twitter:description" content={description} />
+    <meta key="og_img" property="og:image" content={image} />
+    <meta
+      key="twitter_card"
+      name="twitter:card"
+      content="summary_large_image"
+    />
+    <meta key="twitter_img" name="twitter:image" content={image} />
+    <meta key="theme_color" name="theme-color" content={color} />
+    <meta key="tile_color" name="msapplication-TileColor" content={color} />
+    <link
+      key="safari_icon"
+      rel="mask-icon"
+      href="/safari-pinned-tab.svg"
+      color={color}
+    />
+    <link
+      key="apple_icon"
+      rel="apple-touch-icon"
+      sizes="180x180"
+      href="/apple-touch-icon.png"
+    />
+    <link
+      key="favicon_32"
+      rel="icon"
+      type="image/png"
+      sizes="32x32"
+      href="/favicon-32x32.png"
+    />
+    <link
+      key="favicon_16"
+      rel="icon"
+      type="image/png"
+      sizes="16x16"
+      href="/favicon-16x16.png"
+    />
+    <link key="manifest" rel="manifest" href="/site.webmanifest" />
   </Head>
 )
-
